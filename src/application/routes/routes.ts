efafd6b40f6eb2ef6f77b51f49872fe700a19266
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2023-10-24 19:45:43 -0300

    feat: config

---
+++ src/application/routes/routes.ts
@@ -0,0 +1,6 @@
+import { Router as ExpressRouter } from "express";
+
+export const setupRoutes = (router: ExpressRouter) => {
+ 
+  return router;
+};
---
+++ src/application/server.ts
@@ -0,0 +1,25 @@
+import express, { Router, json } from "express";
+import { Environment } from "../shared.kernel/environment";
+
+
+import { setupRoutes } from "./routes/routes";
+
+const router = Router();
+
+export default class Server {
+  private readonly express: express.Application;
+  private readonly port: string = Environment.port;
+
+  constructor() {
+    this.express = express();
+    this.express.use(json());
+  }
+
+  public start = () => {
+    this.express.use(setupRoutes(router))
+
+    this.express.listen(this.port, () => {
+      console.log(`Servidor rodando na porta ${Environment.port} 🏆`);
+    });
+  };
+}
